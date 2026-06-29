0ae9f36993eeba3329a5148442dba2143cbd696c
Mike Kuskov <m.kuskov@innopolis.university>
2026-06-29 19:41:36 +0300

    Clean-up quartus qip ip files

--- synth/zeowaa/ip/virtual_probe/virtual_probe_bb.v
+++
@@ -1,8 +0,0 @@
-
-module virtual_probe (
-	source,
-	probe);	
-
-	output	[63:0]	source;
-	input	[63:0]	probe;
-endmodule
--- synth/zeowaa/ip/virtual_probe/virtual_probe_inst.v
+++
@@ -1,5 +0,0 @@
-	virtual_probe u0 (
-		.source (<connected-to-source>), // sources.source
-		.probe  (<connected-to-probe>)   //  probes.probe
-	);
-
